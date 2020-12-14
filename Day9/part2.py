8770ea2778b9587353946874fd7d3b47d27b88ef
krishnakanthati <krishnakanthati@outlook.com>
2020-12-15 01:58:43 +0530

    Day9

--- Day9/part2.py
+++ Day9/part2.py
@@ -6,20 +6,15 @@ for i in inputs:
     i = int(i.replace('\n', ''))
     li.append(i)
 
-b = 25
+b = 1
 a = 0
 
-z = []
+while True:
+    if sum(li[a: b]) == 731031916:
+        print(min(li[a: b]) + max(li[a: b]))
+        break
+    elif sum(li[a: b]) > 731031916:
+        a += 1
+        b = a + 1
 
-
-while len(li[a: b]) != 24:
-    for i in li[a: b]:
-        for n in li[a: b - 1]:
-            try:
-                if (i + n) == li[b] and li[b] not in z:
-                    z.append(li[b])
-            except IndexError:
-                pass
-
-    a += 1
     b += 1
